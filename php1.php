55ab7e07a885259ab93f94bf4099ab89e8eeb14c
GabLux <gabrielelucchetti@live.it>
2024-01-09 19:49:15 +0100

    esercizio extra svolto

--- php1.php
+++ php1.php
@@ -1,17 +1,18 @@
 <?php
 
-$integer= 1;
+// <- Esercizio 1
 
+$integer = 1;
 $float = 1.5;
-
-$string="la macchina non parte!";
-
-$boolean= true;
+$string = "la macchina non parte!";
+$boolean = true;
 
 var_dump($integer);
 var_dump($float);
 var_dump($string);
 var_dump($boolean);
+
+// <- Esercizio 2
 
 $text1 = "Marco ";
 $text2 = "hai";
@@ -22,78 +23,93 @@ $text6 = $text2;
 $text7 = 'bevuto';
 $text8 = "tutto \n";
 
-
 echo $text1 . " " . $text2 . " " . $text3 . $text4 . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
 
+// <- Esercizio 3
 
-$words1 = [ 
-    'una', 
-    67, 
-    'vita', 
+$words1 = [
+    'una',
+    67,
+    'vita',
     'colle',
-    'mi', 
-    'rosso', 
-    [ 
-      'oscura', 
-      'era', 
-      89, 
-      [ 
-        'mezzo',
-        [ 
-          'cammin', 
-          'Nel', 
-          [ 
-            'selva', 
-            'la', 
-            [ 
-              'via', 
-              'una', 
-              true, 
-            ] 
-          ], 
-        ] 
-      ], 
-    'ritrovai', 
-    'per' 
-    ], 
-    'diritta' 
-  ]; 
-  $words2 = [ 
-    'elemento1' => 25.89, 
-    'elemento2' => 'nostra', 
-    'elemento3' => [ 
-                      'Virgilio', 
-                      "smarrita\n", 
-                      'ché' 
-                    ] 
-  ];
+    'mi',
+    'rosso',
+    [
+        'oscura',
+        'era',
+        89,
+        [
+            'mezzo',
+            [
+                'cammin',
+                'Nel',
+                [
+                    'selva',
+                    'la',
+                    [
+                        'via',
+                        'una',
+                        true,
+                    ]
+                ],
+            ]
+        ],
+        'ritrovai',
+        'per'
+    ],
+    'diritta'
+];
 
+$words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => [
+        'Virgilio',
+        "smarrita\n",
+        'ché'
+    ]
+];
 
-  $words3 = [
+$words3 = [
     'del',
     'di',
     ",",
 
-  ];
+];
 
 $result = $words1[6][3][1][1] . " " . $words1[6][3][0] . " " . $words3[1] . " " . $words1[6][3][1][0] .
- " " . $words3 [1] . " " . $words2['elemento2'] . " " . $words1[2] . " " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " " .
-  $words1[6][3][1][2][2][1] . " " . $words1[6][3][1][2][0] . " " . $words1[6][0] 
-  . $words3[2] . " " . $words2['elemento3'][2] . " " . $words1[6][3][1][2][1] 
-  . " " . $words1[7] . " " . $words1[6][3][1][2][2][0] . " "  . $words1[6][1] . " " . $words2['elemento3'][1];
+    " " . $words3[1] . " " . $words2['elemento2'] . " " . $words1[2] . " " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " " .
+    $words1[6][3][1][2][2][1] . " " . $words1[6][3][1][2][0] . " " . $words1[6][0]
+    . $words3[2] . " " . $words2['elemento3'][2] . " " . $words1[6][3][1][2][1]
+    . " " . $words1[7] . " " . $words1[6][3][1][2][2][0] . " " . $words1[6][1] . " " . $words2['elemento3'][1];
 
 echo $result;
 
+// <- Esercizio 4
 
 $x = 50;
 $y = 20;
 $z = "10";
-$w= 30;
+$w = 30;
 
-var_dump($x<$y);
-var_dump($x<=$w);
-var_dump($y==$z);
-var_dump($y===$z);
-var_dump($y!==$z);
-var_dump($y!=$z);
+var_dump($x < $y);
+var_dump($x <= $w);
+var_dump($y == $z);
+var_dump($y === $z);
+var_dump($y !== $z);
+var_dump($y != $z);
+
+// <- Esercizio 5
+
+$corsoHackademy = [
+    "docenti" => ["Mattia", "Emanuele"],
+    "studente" => ["Gabriele", "Fabio", "Domenico"],
+    "argomenti" => ["HTML,", "CSS,", "Bootstrap,", "JavaScript,", "PHP"],
+];
+
+echo "Sono" . " " . $corsoHackademy["studente"][0] . " e sto studiando ". $corsoHackademy["argomenti"][4] ." con ". $corsoHackademy["docenti"][1] 
+." mentre prima ho imparato ". $corsoHackademy["argomenti"][0] ."". $corsoHackademy["argomenti"][1] ." ". $corsoHackademy["argomenti"][2] 
+. " " . $corsoHackademy["argomenti"][3] ." con " . $corsoHackademy["docenti"][0];
+
+
 
